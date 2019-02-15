1550ecf8b969e4c874420205c031866997fc1f0c
Jokin <327928971@qq.com>
2019-02-15 12:38:05 +0800

    update upload module

--- smms.py
+++ smms.py
@@ -3,13 +3,14 @@
 
 import ssl
 import json
+import base64
 import urllib3
 
 
 class smms:
 
     def __init__(self):
-        print('initializing...')
+        print('SMMS上传类初始化')
         # 关闭证书验证
         ssl._create_default_https_context = ssl._create_unverified_context
         # 取消关闭验证安全提醒
@@ -17,23 +18,34 @@ class smms:
 
     def post(self, filename, file_data, ua='Mozilla/5.0 (Windows NT 10.0; Win64; x64) WallpaperBackuper/1.0.0'):
         print('posting: ' + filename)
+        filename = str(base64.b64encode(filename.encode('utf-8')), 'utf-8')
         http = urllib3.PoolManager()
-        r = http.request(
-            'POST',
-            'https://sm.ms/api/upload',
-            headers={'user-agent': ua},
-            fields={
-                'smfile': (filename, file_data),
-            })
+        try:
+            r = http.request(
+                'POST',
+                'https://sm.ms/api/upload',
+                headers={'user-agent': ua},
+                fields={
+                    'smfile': (filename, file_data),
+                })
+        except Exception:
+            r = {'code': 'error', 'msg': 'Connection failed.'}
         return r
 
     def parseJson(self, r):
-        data = r.data.decode('utf-8')
+        try:
+            data = r.data.decode('utf-8')
+        except Exception:
+            data = str(r)
         print(data)
         if (data.find('Request Entity Too Large') != -1):
-            return {'msg': 'Request Entity Too Large.'}
+            return {'code': 'error', 'msg': 'Request Entity Too Large.'}
         else:
-            return json.loads(data)
+            try:
+                res = json.loads(data)
+            except Exception:
+                res = {'code': 'error', 'msg': 'Bad Json Data.'}
+            return res
 
 
 def test():
